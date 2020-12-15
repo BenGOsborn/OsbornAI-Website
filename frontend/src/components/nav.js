bee95921623b9ebb1d4190aab056e15a79668c71
BenOsborn <bengaoosborn@gmail.com>
2020-12-15 13:02:59 +1100

    Update
    Created react app

---
+++ frontend/src/components/nav.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div className="Landing">
+            <header>
+                <ul>
+                    <b>
+                        <li key="GitHub"><a href="https://github.com/OsbornAI">GitHub</a></li>
+                        <li key="YouTube"><a href="https://www.youtube.com/channel/UCVm_tIEM4uu5HrMT2tG5hVwI">YouTube</a></li>
+                        <li key="LinkedIn"><a href="https://www.linkedin.com/in/OsbornAI/">LinkedIn</a></li>
+                    </b>
+                </ul>
+            </header>
+        </div>
+    );
+}
+
+export default Landing;
