33bf02a42b65973eb657100eb2f3beb8c3dda6a1
BenOsborn <bengaoosborn@gmail.com>
2020-12-21 20:02:36 +1100

    Update
    Added side panel to website
    Pinned header to top
    Rearranged links

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -4,41 +4,36 @@ const Header = () => {
     return (
         <div className="Header">
             <header>
-
+                <ul id="slide-out" class="sidenav">
+                    <li><Link to="About" smooth={true} spy={true} duration={300}>ABOUT</Link></li>
+                    <li><Link to="Services" smooth={true} spy={true} duration={300}>SERVICES</Link></li>
+                    <li><Link to="Book" smooth={true} spy={true} duration={300}>BOOK A CONSULT</Link></li>
+                    <li><Link to="Portfolio" smooth={true} spy={true} duration={300}>PORTFOLIO</Link></li>
+                    <li><a href="https://github.com/OsbornAI" target="_blank" rel="noreferrer">GITHUB</a></li>
+                    <li><a href="https://www.youtube.com/channel/UCVm_tIEM4uu5HrMT2tG5hVw" target="_blank" rel="noreferrer">YOUTUBE</a></li>
+                    <li><a href="https://www.linkedin.com/in/OsbornAI/" target="_blank" rel="noreferrer">LINKEDIN</a></li>
+                </ul>
                 <div class="navbar-fixed">
                     <nav>
                         <div class="nav-wrapper blue darken-3">
                             <a href="/" class="brand-logo center">OSBORNAI</a>
-                            {/* This is meant to be the hamburger icon! */}
-                            <a href="/" data-target="menu" class="sidenav-trigger">MENU</a>
-                            <ul class="left hide-on-med-and-down"> 
-                                <li><Link to="About" smooth={true} spy={true} duration={300}>ABOUT</Link></li>
-                                <li><Link to="Services" smooth={true} spy={true} duration={300}>WHAT WE OFFER</Link></li>
-                                <li><Link to="Book" smooth={true} spy={true} duration={300}>BOOK A CONSULT</Link></li>
-                                <li><Link to="Portfolio" smooth={true} spy={true} duration={300}>PREVIOUS WORK</Link></li>
-                            </ul>
-                            <ul class="right hide-on-med-and-down">
-                                <li><a href="https://github.com/OsbornAI" target="_blank" rel="noreferrer">GITHUB</a></li>
-                                <li><a href="https://www.youtube.com/channel/UCVm_tIEM4uu5HrMT2tG5hVw" target="_blank" rel="noreferrer">YOUTUBE</a></li>
-                                <li><a href="https://www.linkedin.com/in/OsbornAI/" target="_blank" rel="noreferrer">LINKEDIN</a></li>
-                            </ul>
+                            <a href="/" data-target="slide-out" class="sidenav-trigger">MENU</a>
+                            <div class="hide-on-med-and-down">
+                                <ul class="left"> 
+                                    <li><Link to="About" smooth={true} spy={true} duration={300}>ABOUT</Link></li>
+                                    <li><Link to="Services" smooth={true} spy={true} duration={300}>SERVICES</Link></li>
+                                    <li><Link to="Book" smooth={true} spy={true} duration={300}>BOOK A CONSULT</Link></li>
+                                    <li><Link to="Portfolio" smooth={true} spy={true} duration={300}>PORTFOLIO</Link></li>
+                                </ul>
+                                <ul class="right">
+                                    <li><a href="https://github.com/OsbornAI" target="_blank" rel="noreferrer">GITHUB</a></li>
+                                    <li><a href="https://www.youtube.com/channel/UCVm_tIEM4uu5HrMT2tG5hVw" target="_blank" rel="noreferrer">YOUTUBE</a></li>
+                                    <li><a href="https://www.linkedin.com/in/OsbornAI/" target="_blank" rel="noreferrer">LINKEDIN</a></li>
+                                </ul>
+                            </div>
                         </div>
                     </nav>
-
-                    {/* This is not being triggered properly */}
-                    <ul class="sidenav" id="menu">
-                        <li><Link to="About" smooth={true} spy={true} duration={300}>ABOUT</Link></li>
-                        <li><Link to="Services" smooth={true} spy={true} duration={300}>WHAT WE OFFER</Link></li>
-                        <li><Link to="Book" smooth={true} spy={true} duration={300}>BOOK A CONSULT</Link></li>
-                        <li><Link to="Portfolio" smooth={true} spy={true} duration={300}>PREVIOUS WORK</Link></li>
-                        <li><a href="https://github.com/OsbornAI" target="_blank" rel="noreferrer">GITHUB</a></li>
-                        <li><a href="https://www.youtube.com/channel/UCVm_tIEM4uu5HrMT2tG5hVw" target="_blank" rel="noreferrer">YOUTUBE</a></li>
-                        <li><a href="https://www.linkedin.com/in/OsbornAI/" target="_blank" rel="noreferrer">LINKEDIN</a></li>
-                    </ul>
                 </div>
-
-        {/* I want to combine the fixed navbar and the squeezed navbar */}
-
             </header>
         </div>
     );
