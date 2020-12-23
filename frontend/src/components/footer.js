4227f265f00af92feea48d64297355198ce4248e
BenOsborn <bengaoosborn@gmail.com>
2020-12-23 19:06:18 +1100

    Update
    Added security system to website

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-scroll';
-
 const Footer = () => {
     const linkStyle = {
         color: 'white'
@@ -13,6 +11,16 @@ const Footer = () => {
         <div className="Footer">
             <footer class="page-footer grey darken-4">
                 <div class="container center">
+                    <div class="container">
+                        <div class="container">
+                            <div class="container">
+                                <a class="col left" href="https://github.com/OsbornAI" style={linkStyle} target="_blank" rel="noreferrer">GitHub</a>
+                                <a class="col center" href="https://www.youtube.com/channel/UCVm_tIEM4uu5HrMT2tG5hVw" style={linkStyle} target="_blank" rel="noreferrer">YouTube</a>
+                                <a class="col right" href="https://www.linkedin.com/in/OsbornAI/" style={linkStyle} target="_blank" rel="noreferrer">LinkedIn</a>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
                     <div style={copyrightStyle}>
                         © Copyright OsbornAI 2020
                     </div>
@@ -23,12 +31,5 @@ const Footer = () => {
 }
 
 
-{/* <Link class="col left" href="/" style={linkStyle} to="Home" smooth={true} spy={true} duration={300}>Home</Link>
-<Link class="col center" href="/" style={linkStyle} to="Services" smooth={true} spy={true} duration={300}>Services</Link>
-<Link class="col right" href="/" style={linkStyle} to="Book" smooth={true} spy={true} duration={300}>Book A Consult</Link>
-
-<a class="col left" href="https://github.com/OsbornAI" style={linkStyle} target="_blank" rel="noreferrer">GitHub</a>
-<a class="col center" href="https://www.youtube.com/channel/UCVm_tIEM4uu5HrMT2tG5hVw" style={linkStyle} target="_blank" rel="noreferrer">YouTube</a>
-<a class="col right" href="https://www.linkedin.com/in/OsbornAI/" style={linkStyle} target="_blank" rel="noreferrer">LinkedIn</a> */}
 
 export default Footer;
--- frontend/src/pages/book.js
+++ frontend/src/pages/book.js
@@ -1,25 +1,83 @@
+import { useState, useEffect } from 'react';
 import emailjs from 'emailjs-com';
 import './form.css';
 
-const Book = () => {
-    const hookStyle = {
-        fontSize: 23
-    };
+const hookStyle = {
+    fontSize: 23
+};
 
-    const followUpStyle = {
-        fontSize: 18
-    };
+const followUpStyle = {
+    fontSize: 18
+};
+
+const Book = () => {
+    const [display, setDisplay] = useState(0);
+
+    useEffect(() => {
+        const cdDateRaw = localStorage.getItem('Cooldown');
+        const cdDate = JSON.parse(cdDateRaw);
+
+        if (cdDate === null) {
+            setDisplay(0);
+
+        } else {
+            const curDate = new Date().getTime();
+            if (curDate >= cdDate) {
+                setDisplay(0);
+            } else {
+                setDisplay(1);
+            }
+        }
+
+    });
 
     const sendEmail = (e) => {
         e.preventDefault();
 
         emailjs.sendForm('service_cxarmib', 'template-main-id', e.target, 'user_y82ZwKg2eIg9LO4L0jaRq')
         .then((result) => {
-            console.log(result.text);
+            window.M.toast({html: 'Inquiry sent!', displayLength: 5000});
+            localStorage.setItem('Cooldown', JSON.stringify(new Date().getTime() + 10 * 8.64e7)); // This should actually be a date in the future
+            setDisplay(1);
+
         }, (error) => {
-            console.log(error.text);
+            window.M.toast({html: 'Inquiry failed, please try again!', displayLength: 50000});
+            setDisplay(0);
+
         });
-        e.target.reset();
+    };
+
+    const isDisplayed = () => {
+        if (display === 0) {
+            return (
+                <div className="Displayed">
+                    <form onSubmit={sendEmail} id="sendForm">
+                        <div class="input-field">
+                            <input type="text" placeholder="First" name="first" required={true} />
+                            <input type="text" placeholder="Last" name="last" required={true} />
+                            <input type="email" placeholder="Email" name="email" required={true} />
+                            <input type="tel" placeholder="Phone" name="phone" required={true} />
+                            <textarea class="materialize-textarea" id="inquiry" placeholder="Inquiry" name="inquiry" required={true} minLength="10" maxLength="300" />
+                        </div>
+                    </form>
+
+                    <button class="btn blue darken-1 waves-effect waves-light" type="submit" form="sendForm">Send
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+            );
+        } else {
+            return (
+                <div className="Displayed">
+                    <div class="container">
+                        <br />
+                        <p style={{color: '#1E88E5', fontWeight: 'bold'}} class="flow-text">
+                            We're on our way! You can inquire again in {parseInt((JSON.parse(localStorage.getItem('Cooldown')) - new Date().getTime()) / 8.64e7) + 1} days.
+                        </p>
+                    </div>
+                </div>
+            );
+        }
     };
 
     return (
@@ -37,21 +95,7 @@ const Book = () => {
                 Inquire about a consult below so we can get started.
             </p>
 
-            {/* Have a cookie here that acts as a line of defense against people that have already inquired */}
-
-            <form onSubmit={sendEmail} id="sendForm">
-                <div class="input-field">
-                    <input type="text" placeholder="First" name="first" required={true} />
-                    <input type="text" placeholder="Last" name="last" required={true} />
-                    <input type="email" placeholder="Email" name="email" required={true} />
-                    <input type="tel" placeholder="Phone" name="phone" required={true} />
-                    <textarea class="materialize-textarea" id="inquiry" placeholder="Inquiry" name="inquiry" required={true} minLength="10" maxLength="300" />
-                </div>
-            </form>
-
-            <button class="btn blue darken-1 waves-effect waves-light" type="submit" name="action" form="sendForm">Send
-                <i class="material-icons right">send</i>
-            </button>
+            {isDisplayed()}
 
             <br />
             <br />
