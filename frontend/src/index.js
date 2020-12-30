224a84f1bcba2e6a0c39be3d733979b5b6eaf8f9
BenOsborn <bengrantalbertosborn@gmail.com>
2020-12-31 10:55:26 +1100

    Update Changed browser router to hash router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/header';
 import Home from './pages/home';
