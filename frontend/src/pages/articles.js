a819196ab3240d6d9b947caeb687e97bdc61f101
BenOsborn <bengrantalbertosborn@gmail.com>
2020-12-31 16:53:29 +1100

    Update
    UPDATE

--- frontend/src/pages/articles.js
+++ frontend/src/pages/articles.js
@@ -7,10 +7,34 @@ import { ArticleCard } from './articles-page';
 import TestArticle from './articles/test-article';
 
 const articleMetadata = {
-    'test-one': {'component': TestArticle(), 'title': 'Article 1', 'author': 'OsbornAI', 'description': 'Art1', 'keywords': 'ArtKeyword1', 'date_published': 'Dec 30'},
-    'test-two': {'component': TestArticle(), 'title': 'Article 2', 'author': 'OsbornAI', 'description': 'Art2', 'keywords': 'ArtKeyword2', 'date_published': 'Dec 30'},
-    'test-three': {'component': TestArticle(), 'title': 'Article 3', 'author': 'OsbornAI', 'description': 'Art3', 'keywords': 'ArtKeyword3', 'date_published': 'Dec 31'},
+    'test-twentyeight': {'component': TestArticle(), 'title': 'Article 4', 'author': 'OsbornAI', 'description': 'Art4', 'keywords': 'ArtKeyword4', 'date_published': 'Dec 31'},
+    'test-twentyseven': {'component': TestArticle(), 'title': 'Article 3', 'author': 'OsbornAI', 'description': 'Art3', 'keywords': 'ArtKeyword3', 'date_published': 'Dec 31'},
+    'test-twentysix': {'component': TestArticle(), 'title': 'Article 2', 'author': 'OsbornAI', 'description': 'Art2', 'keywords': 'ArtKeyword2', 'date_published': 'Dec 30'},
+    'test-twentyfive': {'component': TestArticle(), 'title': 'Article 1', 'author': 'OsbornAI', 'description': 'Art1', 'keywords': 'ArtKeyword1', 'date_published': 'Dec 30'},
+    'test-twentyfour': {'component': TestArticle(), 'title': 'Article 4', 'author': 'OsbornAI', 'description': 'Art4', 'keywords': 'ArtKeyword4', 'date_published': 'Dec 31'},
+    'test-twentythree': {'component': TestArticle(), 'title': 'Article 3', 'author': 'OsbornAI', 'description': 'Art3', 'keywords': 'ArtKeyword3', 'date_published': 'Dec 31'},
+    'test-twentytwo': {'component': TestArticle(), 'title': 'Article 2', 'author': 'OsbornAI', 'description': 'Art2', 'keywords': 'ArtKeyword2', 'date_published': 'Dec 30'},
+    'test-twentyone': {'component': TestArticle(), 'title': 'Article 1', 'author': 'OsbornAI', 'description': 'Art1', 'keywords': 'ArtKeyword1', 'date_published': 'Dec 30'},
+    'test-twenty': {'component': TestArticle(), 'title': 'Article 4', 'author': 'OsbornAI', 'description': 'Art4', 'keywords': 'ArtKeyword4', 'date_published': 'Dec 31'},
+    'test-nineteen': {'component': TestArticle(), 'title': 'Article 3', 'author': 'OsbornAI', 'description': 'Art3', 'keywords': 'ArtKeyword3', 'date_published': 'Dec 31'},
+    'test-eighteen': {'component': TestArticle(), 'title': 'Article 2', 'author': 'OsbornAI', 'description': 'Art2', 'keywords': 'ArtKeyword2', 'date_published': 'Dec 30'},
+    'test-seventeen': {'component': TestArticle(), 'title': 'Article 1', 'author': 'OsbornAI', 'description': 'Art1', 'keywords': 'ArtKeyword1', 'date_published': 'Dec 30'},
+    'test-sixteen': {'component': TestArticle(), 'title': 'Article 4', 'author': 'OsbornAI', 'description': 'Art4', 'keywords': 'ArtKeyword4', 'date_published': 'Dec 31'},
+    'test-fifteen': {'component': TestArticle(), 'title': 'Article 3', 'author': 'OsbornAI', 'description': 'Art3', 'keywords': 'ArtKeyword3', 'date_published': 'Dec 31'},
+    'test-fourteen': {'component': TestArticle(), 'title': 'Article 2', 'author': 'OsbornAI', 'description': 'Art2', 'keywords': 'ArtKeyword2', 'date_published': 'Dec 30'},
+    'test-thirteen': {'component': TestArticle(), 'title': 'Article 1', 'author': 'OsbornAI', 'description': 'Art1', 'keywords': 'ArtKeyword1', 'date_published': 'Dec 30'},
+    'test-twelve': {'component': TestArticle(), 'title': 'Article 4', 'author': 'OsbornAI', 'description': 'Art4', 'keywords': 'ArtKeyword4', 'date_published': 'Dec 31'},
+    'test-eleven': {'component': TestArticle(), 'title': 'Article 3', 'author': 'OsbornAI', 'description': 'Art3', 'keywords': 'ArtKeyword3', 'date_published': 'Dec 31'},
+    'test-ten': {'component': TestArticle(), 'title': 'Article 2', 'author': 'OsbornAI', 'description': 'Art2', 'keywords': 'ArtKeyword2', 'date_published': 'Dec 30'},
+    'test-nine': {'component': TestArticle(), 'title': 'Article 1', 'author': 'OsbornAI', 'description': 'Art1', 'keywords': 'ArtKeyword1', 'date_published': 'Dec 30'},
+    'test-eight': {'component': TestArticle(), 'title': 'Article 4', 'author': 'OsbornAI', 'description': 'Art4', 'keywords': 'ArtKeyword4', 'date_published': 'Dec 31'},
+    'test-seven': {'component': TestArticle(), 'title': 'Article 3', 'author': 'OsbornAI', 'description': 'Art3', 'keywords': 'ArtKeyword3', 'date_published': 'Dec 31'},
+    'test-six': {'component': TestArticle(), 'title': 'Article 2', 'author': 'OsbornAI', 'description': 'Art2', 'keywords': 'ArtKeyword2', 'date_published': 'Dec 30'},
+    'test-five': {'component': TestArticle(), 'title': 'Article 1', 'author': 'OsbornAI', 'description': 'Art1', 'keywords': 'ArtKeyword1', 'date_published': 'Dec 30'},
     'test-four': {'component': TestArticle(), 'title': 'Article 4', 'author': 'OsbornAI', 'description': 'Art4', 'keywords': 'ArtKeyword4', 'date_published': 'Dec 31'},
+    'test-three': {'component': TestArticle(), 'title': 'Article 3', 'author': 'OsbornAI', 'description': 'Art3', 'keywords': 'ArtKeyword3', 'date_published': 'Dec 31'},
+    'test-two': {'component': TestArticle(), 'title': 'Article 2', 'author': 'OsbornAI', 'description': 'Art2', 'keywords': 'ArtKeyword2', 'date_published': 'Dec 30'},
+    'test-one': {'component': TestArticle(), 'title': 'Article 1', 'author': 'OsbornAI', 'description': 'Art1', 'keywords': 'ArtKeyword1', 'date_published': 'Dec 30'},
 };
 
 // This is going to render our raw HTML components
