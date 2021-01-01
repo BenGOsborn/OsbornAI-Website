282583a6ca7ff01ee4ce4e244274f6360a704735
BenOsborn <bengrantalbertosborn@gmail.com>
2021-01-01 15:58:38 +1100

    Update
    Cleaned app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/header';
@@ -28,8 +27,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
