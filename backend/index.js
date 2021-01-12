081bd6bd26dee88e8e4d009bef0e60bf2bec9529
BenOsborn <bengrantalbertosborn@gmail.com>
2021-01-12 22:37:20 +1100

    Update

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config();
 const pay = require('./routes/pay');
+const inquire = require('./routes/inquire');
 
 const app = express();
 
 app.use('/pay', pay);
+app.use('/inquire', inquire);
 
 app.listen(8000);
--- backend/routes/inquire.js
+++ backend/routes/inquire.js
@@ -1,7 +1,6 @@
-const e = require('express');
 const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
 router.post('/', (req, res) => {
     
