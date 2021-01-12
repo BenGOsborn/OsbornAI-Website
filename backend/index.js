fcc3b6f0c1455b32e561142625cfbd1dc3c95d34
BenOsborn <bengrantalbertosborn@gmail.com>
2021-01-12 22:13:31 +1100

    Update

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const pay = require('./routes/pay');
 
 const app = express();
--- backend/routes/pay.js
+++ backend/routes/pay.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send("We are on payment!")
+router.post('/', (req, res) => {
+    try {
+        const { product, token } = req.body;
+
+        const customer = await stripe.customers.create({
+            email: token.email,
+            source: token.id
+        });
+
+        const idempotency_key = uuidv4();
+        const charge = await stripe.charges.create({
+            amount: product.price * 100,
+            currency: 'aud',
+            customer: customer.id,
+            receipt_email: token.email,
+        },
+        {
+            idempotency_key
+        });
+        const status = 1;
+        const error = null;
+
+    } catch (e) {
+        const status = 0;
+        const error = e;
+    };
+
+    res.json({
+        'status': status,
+        'error': error
+    });
 });
 
 module.exports = router;
