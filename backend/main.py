0ee48cbeeb07e57cfed7639ddc6a687280fe60eb
BenOsborn <bengrantalbertosborn@gmail.com>
2021-01-13 12:53:10 +1100

    Update

---
+++ backend/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+import stripe
+import os
+
+app = Flask(__name__)
+
+stripe.api_key = os.getenv('STRIPE_SECRET')
+
+@app.route('/pay/:pay_token', methods=['POST'], strict_slashes=False)
+def pay():
+    # This is where we will get the pay request information sent by the token which will contain the amount, the type of item and such
+    amount = None
+
+    form_json = request.get_json()
+    email = form_json['email']
+
+    # Add more information here - Description to be echoed back to the user
+    intent = stripe.PaymentIntent.create(
+        amount=amount * 100,
+        currency='aud',
+        receipt_email=email
+    )
+
+    return jsonify({'client_secret': intent['client_secret']})
+
+# Add this webhook once the app has been deployed
+@app.route('/payment_webook', methods=['POST'], strict_slashes=False)
+def paymentWebhook():
+    event = request.get_json()
+
+    if event['type'] == 'payment_intent.succeeded':
+        user_email = event['data']['object']['receipt_email']
+
+        # Send an email here then return success
+        # Send a custom email template
+
+        return jsonify({'success': 1})
+
+    else:
+        return jsonify({'success': 0})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
--- frontend/src/pages/articles/article-metadata.js
+++ frontend/src/pages/articles/article-metadata.js
@@ -2,8 +2,8 @@ import RNNTextGeneration from './text-generation-using-a-rnn/text-generation-usi
 import DeepFakeFaceSwap from './deep-fake-face-swap-autoencoder/deep-fake-face-swap-autoencoder';
 
 const articleMetadata = {
-    'text-generation-using-a-rnn': {'component': RNNTextGeneration(), 'title': 'Text Generation Using A RNN', 'author': 'OsbornAI', 'description': 'This article demonstrates how a recurrent neural network can be used to generate text one character at a time.', 'keywords': 'rnn, character generation, text generation, lstm, text generator, gru, tensorflow', 'date_published': '02/01/2021'},
     'deep-fake-face-swap-autoencoder': {'component': DeepFakeFaceSwap(), 'title': 'Deep Fake Face Swap Autoencoder', 'author': 'OsbornAI', 'description': 'This article demonstrates how an autoencoder can be used to create a deep fake that swaps the faces of an individual with another.', 'keywords': 'autoencoder, face swap, deep fake, tensorflow, keras, tutorial', 'date_published': '11/01/2021'},
+    'text-generation-using-a-rnn': {'component': RNNTextGeneration(), 'title': 'Text Generation Using A RNN', 'author': 'OsbornAI', 'description': 'This article demonstrates how a recurrent neural network can be used to generate text one character at a time.', 'keywords': 'rnn, character generation, text generation, lstm, text generator, gru, tensorflow', 'date_published': '02/01/2021'},
 };
 
 export default articleMetadata;
