753f8887c20537c66b3a014c1e9f338f5f5ebde0
BenOsborn <bengrantalbertosborn@gmail.com>
2021-01-15 11:19:22 +1100

    Update

--- frontend/src/components/book.js
+++ frontend/src/components/book.js
@@ -2,70 +2,79 @@ import { useState, useEffect } from 'react';
 import '../pages/form.css';
 import axios from 'axios';
 
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_cxarmib', 'template-main-id', e.target, 'user_y82ZwKg2eIg9LO4L0jaRq')
+    .then((result) => {
+        window.M.toast({html: 'Inquiry sent!', displayLength: 5000});
+        localStorage.setItem('Cooldown', JSON.stringify(new Date().getTime() + 10 * 8.64e7)); // This should actually be a date in the future
+        setDisplay(1);
+
+    }, (error) => {
+        window.M.toast({html: 'Inquiry failed, please try again!', displayLength: 5000});
+        setDisplay(0);
+
+    });
+};
+
 const Book = () => {
-    const [display, setDisplay] = useState(0);
+    const [daysSince, setDaysSince] = useState(0);
+
+    const [first, setFirst] = useState(null);
+    const [last, setLast] = useState(null);
+    const [email, setEmail] = useState(null);
+    const [inquiry, setInquiry] = useState(null);
+
+    const daysSince = (last_inquiry_raw) => {
+        const current_date = new Date().getTime();
+        const last_inquiry = new Date(last_inquiry_raw);
+        const days_since = parseInt((current_date - last_inquiry) / 8.64e7) + 1;
+
+        return days_since;
+    };
 
     useEffect(() => {
         const last_inquiry_raw = localStorage.getItem('last_inquiry');
-        const last_inquiry = JSON.parse(last_inquiry_raw); // Does this get converted to a date in milliseconds?
 
-        if (last_inquiry === null) {
-            setDisplay(0);
+        if (last_inquiry_raw === null) {
+            setDaysSince(0);
         } else {
-            const current_date = new Date().getTime();
-            const days_since = (current_date - last_inquiry) / 8.64e7
-            if (days_since < 10) {
-                setDisplay(0);
-            } else {
-                setDisplay(1);
-            }
+            const days_since = daysSince(last_inquiry_raw);
+            setDaysSince(days_since);
         }
     }, []);
 
     const sendInquiry = (e) => {
         e.preventDefault();
 
-        axios.get("https://osbornai.herokuapp.com/add_inquiry")
+        axios.post("https://osbornai.herokuapp.com/add_inquiry", {'first': first, 'last': last, 'email': email, 'inquiry': inquiry})
         .then(res => {
             const form = res.data;
-            const last_inquiry = new Date(form.last_inquiry);
-            // Now we'll set the token to be this last inquiry date, and then we'll have to do another setDisplay to update it
-            // We should also store the days left in a state variable
-        });
 
+            const last_inquiry = form.last_inquiry;
+            localStorage.setItem('last_inquiry', last_inquiry);
+
+            const days_since = daysSince(last_inquiry);
+            setDaysSince(days_since);
+        });
     }; 
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs.sendForm('service_cxarmib', 'template-main-id', e.target, 'user_y82ZwKg2eIg9LO4L0jaRq')
-        .then((result) => {
-            window.M.toast({html: 'Inquiry sent!', displayLength: 5000});
-            localStorage.setItem('Cooldown', JSON.stringify(new Date().getTime() + 10 * 8.64e7)); // This should actually be a date in the future
-            setDisplay(1);
-
-        }, (error) => {
-            window.M.toast({html: 'Inquiry failed, please try again!', displayLength: 5000});
-            setDisplay(0);
-
-        });
-    };
-
+    // Have this update on the display change
     const isDisplayed = () => {
-        if (display === 0) {
+        if (daysSince < 10) {
             return (
                 <div className="Displayed">
-                    <form onSubmit={sendEmail} id="sendForm">
+                    <form onSubmit={sendInquiry} id="sendForm">
                         <div class="input-field">
-                            <input type="text" placeholder="First" name="first" required={true} />
-                            <input type="text" placeholder="Last" name="last" required={true} />
-                            <input type="email" placeholder="Email" name="email" required={true} />
-                            <input type="tel" placeholder="Phone" name="phone" required={true} />
-                            <textarea class="materialize-textarea" id="inquiry" placeholder="Inquiry" name="inquiry" required={true} minLength="10" maxLength="300" />
+                            <input type="text" placeholder="First" name="first" required={true} onChange={(e) => {setFirst(e.target.value)}} />
+                            <input type="text" placeholder="Last" name="last" required={true} onChange={(e) => {setLast(e.target.value)}} />
+                            <input type="email" placeholder="Email" name="email" required={true} onChange={(e) => {setEmail(e.target.value)}} />
+                            <textarea class="materialize-textarea" id="inquiry" placeholder="Inquiry" name="inquiry" required={true} minLength="10" maxLength="300" onChange={(e) => {setInquiry(e.target.value)}} />
                         </div>
                     </form>
-
-                    <button class="btn blue darken-1 waves-effect waves-light" type="submit" form="sendForm">Send
+                    <button class="btn blue darken-1 waves-effect waves-light" type="submit" form="sendForm">
+                        Send
                         <i class="material-icons right">send</i>
                     </button>
                 </div>
@@ -76,7 +85,7 @@ const Book = () => {
                     <div class="container">
                         <br />
                         <p style={{color: '#1E88E5', fontWeight: 'bold'}} class="flow-text">
-                            We'll contact you shortly! You may reinquire in {parseInt((JSON.parse(localStorage.getItem('Cooldown')) - new Date().getTime()) / 8.64e7) + 1} days.
+                            We'll contact you shortly! You may reinquire in {10 - daysSince} days.
                         </p>
                     </div>
                 </div>
