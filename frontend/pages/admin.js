da7b88854b2fbbc00607a2eb1a75da91628525bf
BenOsborn <bengrantalbertosborn@gmail.com>
2021-02-02 23:38:03 +1100

    Update

---
+++ frontend/pages/admin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+export default function Admin(props) {
+
+};
+
+export default async function getStaticProps() {
+    // First we will try to initially log in here, if we cannot then we will have to call  the function
+
+    return {
+        params: {
+            
+        }
+    };
+};
