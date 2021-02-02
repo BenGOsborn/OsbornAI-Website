7965b400df201f9ff22f1275926e649b6249b437
BenOsborn <bengrantalbertosborn@gmail.com>
2021-02-02 16:02:51 +1100

    Update

--- frontend/pages/articles/[slug].js
+++ frontend/pages/articles/[slug].js
@@ -1,11 +1,32 @@
 import React from 'react';
 import fs from 'fs';
 
-export default function Article(props) {
+export default function Article({ slug }) {
     return (
         <div>
-            I am a {props.slug}!
+            I am a {slug}!
         </div>
     );
 };
 
+export async function getStaticPaths() {
+    const files = fs.readdirSync('articles');
+    const paths = files.map(filename => ({
+        params: {
+            slug: filename.replace('.md', '')
+        }
+    }));
+
+    return {
+        paths,
+        fallback: false
+    };
+};
+
+export async function getStaticProps({ params: { slug } }) {
+    return {
+        props: {
+            slug
+        }
+    };
+};
