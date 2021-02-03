0822f22bfbca8b280b7c2de61ae5e56c2be9d6c2
BenOsborn <bengrantalbertosborn@gmail.com>
2021-02-03 15:23:40 +1100

    Update

--- frontend/pages/admin/index.js
+++ frontend/pages/admin/index.js
@@ -1,33 +1,59 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 export default function Admin(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [failed, setFailed] = useState(false);
+
+    const router = useRouter();
 
     function onSubmit(e) {
         e.preventDefault();
 
-        axios.post('/api/admin-login', { username: username, password: password }).then((res) => console.log(res)).catch((err) => console.log(err));
+        axios.post('/api/admin-login', { username: username, password: password })
+        .then((res) => {
+            router.push("/admin/dashboard");
+
+            setFailed(false);
+        })
+        .catch((err) => {
+            setFailed(true);
+        });
     };
 
     return (
-        <>
-            <div className="AdminLogin">
-                <div className="container">
-                    <h1>Admin Login</h1>
-                    <form onSubmit={onSubmit} id="login">
-                        <div className="input-field">
-                            <input type="text" placeholder="Username" name="username" required={true} onChange={(e) => {setUsername(e.target.value)}} />
-                            <input type="password" placeholder="Password" name="password" required={true} onChange={(e) => {setPassword(e.target.value)}} />
-                        </div>
-                    </form>
-                    <button className="btn blue darken-1 waves-effect waves-light" type="submit" form="login">
-                        Login
-                        <i className="material-icons right">send</i>
-                    </button>
-                </div>
+        <div className="AdminLogin">
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="container">
+                <h1 className="center">Admin Login</h1>
+                <form onSubmit={onSubmit} id="login">
+                    <div className="input-field">
+                        <input type="text" placeholder="Username" name="username" required={true} onChange={(e) => {setUsername(e.target.value)}} />
+                        <input type="password" placeholder="Password" name="password" required={true} onChange={(e) => {setPassword(e.target.value)}} />
+                    </div>
+                </form>
+                <button className="btn blue darken-1 waves-effect waves-light" type="submit" form="login">
+                    Login
+                    <i className="material-icons right">send</i>
+                </button>
+                {failed === true ? <p className="flow-text" style={{color: 'red'}}>Login failed. Please try again!</p> : <></>}
             </div>
-        </>
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
     );
+};
+
+export function getServerSideProps({ req, res }) {
+    const token = req.cookies.token;
+    // Now we need to validate if this token is correct or not before redirecting
+
+    return { props: {  } }
 };
--- frontend/pages/api/admin-login.js
+++ frontend/pages/api/admin-login.js
@@ -2,18 +2,23 @@ import cookie from 'cookie';
 import axios from 'axios';
 
 export default function AdminLogin(req, res) {
-    axios.post('https://osbornai.herokuapp.com/admin/login', { username: req.body.username, password: req.body.password }).then((res) => console.log(res.data)).catch((err) => console.log(err.response));
-
-    // res.setHeader(
-    //     "Set-Cookie", 
-    //     cookie.serialize("token", "IAMATOKEN", {
-    //         httpOnly: true,
-    //         secure: process.env.NODE_ENV !== 'development',
-    //         maxAge: 60 * 60 * 24,
-    //         sameSite: "strict",
-    //         path: "/"
-    //     })
-    // );
-    res.statusCode = 200; 
-    res.json({ success: true });
+    axios.post('https://osbornai.herokuapp.com/admin/login', { username: req.body.username, password: req.body.password })
+    .then((response) => {
+        res.setHeader(
+            "Set-Cookie", 
+            cookie.serialize("token", response.data.token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== 'development',
+                maxAge: 60 * 60 * 24,
+                sameSite: "strict",
+                path: "/"
+            })
+        );
+        res.statusCode = 200; 
+        res.json({ success: true });
+    })
+    .catch((err) => {
+        res.statusCode = 400; 
+        res.json({ success: false });
+    });
 };
