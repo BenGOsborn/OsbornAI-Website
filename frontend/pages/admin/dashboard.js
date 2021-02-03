f82d6272a756bb04937dc7806f4dec7ce574e1c8
BenOsborn <bengrantalbertosborn@gmail.com>
2021-02-03 23:12:45 +1100

    Update

--- frontend/pages/admin/dashboard.js
+++ frontend/pages/admin/dashboard.js
@@ -1,6 +1,16 @@
+import React, { useState } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 export default function Dashboard({ redirect, inquiry_notifications, payments, payment_ids }) {
+    const router = useRouter();
+
+    React.useEffect(() => {
+        if (redirect) {
+            router.push('/admin');
+        } 
+    });
+
     return (
         <div className="Dashboard">
             <h1>Dashboard</h1>
@@ -11,40 +21,37 @@ export default function Dashboard({ redirect, inquiry_notifications, payments, p
 export async function getServerSideProps({ req, res }) {
     const token = req.cookies.token;
 
-    // try {
-    //     await axios.post('http://127.0.0.1:5000/admin/validate_token', { token: token });
-    // } catch (err) {
-    //     console.log(1);
-    //     console.log(err.response.data);
-    // }
+    let return_form = {
+        redirect: true,
+        inquiry_notifications: [],
+        payments: [],
+        payment_ids: []
+    };
 
     try {
-
-        const inquiry_notification_response = await axios.post('http://127.0.0.1:5000/admin/view_inquiry_notifications', { token: token });
-    } catch (err) {
-        console.log(2);
-        console.log(err.response.data);
+        await axios.post('https://osbornai.herokuapp.com/admin/validate_token', { token: token });
+        return_form.redirect = false;
+    } catch {
+        return { props: return_form };
     }
 
-    // try {
-    //     const payments_response = await axios.get('https://osbornai.herokuapp.com/admin/view_payments', { token: token });
-    // } catch (err) {
-    //     console.log(3);
-    //     console.log(err.response.data);
+    try {
+        const inquiry_notifications_response = await axios.post('https://osbornai.herokuapp.com/admin/view_inquiry_notifications', { token: token });
+        return_form.inquiry_notifications = inquiry_notifications_response.data.inquiry_notifications;
+    } catch { }
 
-    // }
+    try {
+        const payments_response = await axios.post('https://osbornai.herokuapp.com/admin/view_payments', { token: token });
+        return_form.payments = payments_response.data.payments;
+    } catch { }
 
-    // try {
-    //     const payment_ids_response = await axios.get('https://osbornai.herokuapp.com/admin/view_valid_payment_ids', { token: token });
-    // } catch (err) {
-    //     console.log(4);
-    //     console.log(err.response.data);
-    // }
+    try {
+        const payment_ids_response = await axios.post('https://osbornai.herokuapp.com/admin/view_valid_payment_ids', { token: token });
+        return_form.payment_ids = payment_ids_response.data.payment_ids;
+    } catch { }
 
     return { 
-        props: { 
-            redirect: false, 
-        } 
+        props: { ...return_form } 
     };
 
 };
