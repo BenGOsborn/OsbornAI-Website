238b29da71de0b61ae5d99a6f164289e5415d8e4
BenOsborn <bengrantalbertosborn@gmail.com>
2021-02-10 20:56:48 +1100

    Update

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -47,7 +47,7 @@ export default function Layout(props) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="theme-color" content="#1E88E5" />
                 <meta name="description" content="We use data and AI to help you grow your business. If you have a project in mind that involves lots of data or are looking to grow your business, then book a consult with us, and let's get started!" />
-				<meta name="keywords" content="data analytics, business analytics, data dashboards, marketing analysis, business consulting, osbornai, machine learning, data science, artificial intelligence, ai, data" />
+                <meta name="keywords" content="data analytics, business analytics, data dashboards, marketing analysis, business consulting, osbornai, machine learning, data science, artificial intelligence, ai, data" />
                 <meta name="robots" content="index, follow" />
                 <title>Grow Your Business Using Data and AI - OsbornAI</title>
 
