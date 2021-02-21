1b1f044696fae9dc6fd924694cb1fe87f28f62ca
BenOsborn <bengrantalbertosborn@gmail.com>
2021-02-21 13:57:27 +1100

    Update

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -48,7 +48,7 @@ export default function Home(props) {
 						<p style={{fontSize: 20}}>
 							Here at OsbornAI, we understand the importance of data, so we make it our mission 
 							to help you use data in the most effective way for your business, so you can make 
-							smarter business decisions and tackle complex problems within your business. 
+							smarter business decisions and tackle complex problems within your business.
 						</p>
 					</div>
 				</span>
@@ -63,48 +63,89 @@ export default function Home(props) {
 							</p>
 						</b>
 						<p style={{fontSize: 18}}>
-							Whether you want to know how data can help improve your business, or you have lots of data that you don’t know what to do with, we can help you.
-							Here's some of the service's we provide:
+							Whether you want to know how data can help improve your business, or your growing business is collecting too much data that you don't know what to do with, we 
+							will work with you to find the best course of action to achieve your desired outcome and then implement these solutions for you quickly and in a way that will 
+							scale for however large your business may grow. Here's just a few of the problems we can help you with:
 						</p>
 						<div className="row">
+							<div className="col s12 m6 l6">
+								<div className="card">
+									<div className="card-image">
+										<img alt="Black and Gray Laptop on Black Sectional Couch" src="https://images.pexels.com/photos/577210/pexels-photo-577210.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+									</div>
+									<div className="card-content">
+										<span className="card-title">
+											<b>Data Analysis and Insight Extraction</b>
+										</span>
+										<p>
+											Got lots of data but no idea what to do with it? We'll transform your raw useless data into meaningful insights, reports
+											and visualizations that will allow you to understand your target audience and customer behaviours, helping you to make smarter business decisions 
+											and understand and focus on critical areas within your business.
+										</p>
+									</div>
+								</div>
+							</div>
+							<div className="col s12 m6 l6">
+								<div className="card">
+									<div className="card-image">
+										<img alt="Photo of Person Typing on Computer Keyboard" src="https://images.pexels.com/photos/735911/pexels-photo-735911.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+									</div>
+									<div className="card-content">
+										<span className="card-title">
+											<b>Data Collection and Metric Tracking</b>
+										</span>
+										<p>Looking to integrate data and analytics into your business but not sure how? We'll help you to find ways to collect data and track your users.
+											you can use.
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="row">
 							<div className="col s12 m4 l4">
-							<div className="card">
-								<div className="card-image">
-									<img alt="Person Holding Space Gray Iphone 6" src="https://images.pexels.com/photos/17663/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+								<div className="card">
+									<div className="card-image">
+										<img alt="Server racks in modern data center" src="https://images.pexels.com/photos/4508751/pexels-photo-4508751.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+									</div>
+									<div className="card-content">
+										<span className="card-title">
+											<b>Data Storage Solutions</b>
+										</span>
+										<p>In need of a new place to store your data? We'll work with you to find and implement the storage solution that will suit you and your business in the most 
+											cost effective and scalable way.
+										</p>
+									</div>
 								</div>
-								<div className="card-content">
-									<span className="card-title">
-										<b>Consulting</b>
-									</span>
-									<p>We'll work with you to find ways in which data can help your business grow, then implement these systems for you.</p>
-								</div>
-							</div>
 							</div>
 							<div className="col s12 m4 l4">
-							<div className="card">
-								<div className="card-image">
-									<img alt="Black and Gray Laptop on Black Sectional Couch" src="https://images.pexels.com/photos/577210/pexels-photo-577210.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+								<div className="card">
+									<div className="card-image">
+										<img alt="Man Beside Woman Billboard" src="https://images.pexels.com/photos/1031700/pexels-photo-1031700.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+									</div>
+									<div className="card-content">
+										<span className="card-title">
+											<b>Advertising Campaigns</b>
+										</span>
+										<p>Got a marketing campaign that isn't as effective as you'd like, or looking to start a campaign in a way that will maximize your return while minimizing your expenses?
+											We'll work with you to help you craft more effective campaigns that will directly target your main demographic, increasing your campaign effectiveness!
+										</p>
+									</div>
 								</div>
-								<div className="card-content">
-									<span className="card-title">
-										<b>Data Analysis</b>
-									</span>
-									<p>We'll extract crucial insights about your business from your data and present them to you concisely, so you know exactly what areas of your business to focus.</p>
-								</div>
-							</div>
 							</div>
 							<div className="col s12 m4 l4">
-							<div className="card">
-								<div className="card-image">
-									<img alt="Time Lapse Photography of Blue Lights" src="https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260s" />
+								<div className="card">
+									<div className="card-image">
+										<img alt="Assorted-color Plastic Gear Lot" src="https://images.pexels.com/photos/1476320/pexels-photo-1476320.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+									</div>
+									<div className="card-content">
+										<span className="card-title">
+											<b>Automation</b>
+										</span>
+										<p>Spending too much time or money on an area of your business that involves repetitive tasks? Chances are we can find a way to automate it for you using 
+											state of the art machine learning and deep learning algorithms saving you time and money in the long run.
+										</p>
+									</div>
 								</div>
-								<div className="card-content">
-									<span className="card-title">
-										<b>Machine Learning and AI</b>
-									</span>
-									<p>We'll use cutting edge Machine Learning algorithms to extract hidden insights from your data and to automate areas of your business, saving you money.</p>
-								</div>
-							</div>
 							</div>
 						</div>
 						<p style={{fontSize: 18}}>
